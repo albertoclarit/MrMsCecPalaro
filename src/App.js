bbe98cb90967c69c620f0707885b160c929d136b
john bill Suarez <john bill Suarez>
2016-08-12 16:33:47 +0800

    tried routing between pages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,65 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+//import ReactDOM from 'react-dom';
+import {Well,
+        FormGroup,
+        FormControl,
+        ControlLabel,
+        HelpBlock,
+        Button,
+        ButtonGroup} from 'react-bootstrap';
+import './styles/bootstrap.css';
 
-class App extends Component {
-  render() {
+export default class App extends React.Component {
+    //eslint-disable-next-line
+   constructor(props){
+       super(props);
+   }
+    goToHome(){
+        this.props.history.push("/home");
+    }
+   
+  render(){
+      
+        const wellStyle={
+          width: 400,
+          height: 500,
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          marginTop: 50
+        };
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div className="container">
+        <Well style={wellStyle}>
+          <legend>Welcome to GeminiTech</legend>
+          <form>
+              <FormGroup>
+                    <ControlLabel> Enter Username </ControlLabel>
+                          <FormControl
+                              type='text'
+                              placeholder='Enter Your Username'/>
+                    <FormControl.Feedback/>
+                        <HelpBlock> </HelpBlock>
+              </FormGroup>
+              <FormGroup>
+                        <ControlLabel>Enter Password </ControlLabel>
+                    <FormControl
+                        type='password'
+                        placeholder='Enter Your Password'/>
+                <FormControl.Feedback/>
+                    <HelpBlock> </HelpBlock>
+                </FormGroup>
+                <div className="button">
+                    <ButtonGroup>
+                        <Button  bsStyle="default" type="submit" onClick={this.goToHome.bind(this)}>
+                        Login</Button>
+                        <Button  bsStyle="primary" type="button">
+                        Register</Button>
+                    </ButtonGroup>    
+                </div>
+            </form>
+         </Well>
+         </div>
     );
   }
 }
 
-export default App;
